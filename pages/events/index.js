dc4e4e2a77f8a7e65f7009ca17f8bf95a71627ee
wooyoung.kim <uyfly@naver.com>
2023-10-22 23:56:32 +0900

    Add submit handler

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,13 +2,20 @@ import { getAllEvents } from "@/dummy-data";
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 const AllEventPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
