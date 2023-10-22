96b95c115b30e2b914b3eedf27a1e15185bbc24c
wooyoung.kim <uyfly@naver.com>
2023-10-22 23:57:59 +0900

    Add event filter conditional clause

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,38 @@
+import { getFilteredEvents } from "@/dummy-data";
 import { useRouter } from "next/router";
 
 const FilteredEventsPage = () => {
   const router = useRouter();
 
-  console.log(router.query);
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>; // className이 문자열인 이유는 globals.css에 정의된 style이기 때문
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
