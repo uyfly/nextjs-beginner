65685709b78809caa3a1a8974c04ab7578a6fc20
wooyoung.kim <uyfly@naver.com>
2023-11-02 12:32:43 +0900

    Change file path

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ function HomePage(props) {
       <ul>
         {products.map((product) => (
           <li key={product.id}>
-            <Link href={`/${product.id}`}>{product.title}</Link>
+            <Link href={`/products/${product.id}`}>{product.title}</Link>
           </li>
         ))}
       </ul>
