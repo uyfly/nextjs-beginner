f52ca405431650ab05e79902cc30b12751489871
wooyoung.kim <uyfly@naver.com>
2023-11-03 22:48:01 +0900

    study off day..

--- pages/404.js
+++ pages/404.js
@@ -2,6 +2,10 @@ function NotFoundPage() {
   return (
     <div>
       <h1>Page not found!</h1>
+      <h2>Study off day</h2>
+      <ul>
+        <li>2023-11-03</li>
+      </ul>
     </div>
   );
 }
