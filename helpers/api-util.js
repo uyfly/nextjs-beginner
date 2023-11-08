be27d14a8fd8d46d818f21caeee7c5379af94de4
wooyoung.kim <uyfly@naver.com>
2023-11-08 21:53:51 +0900

    Add api util

---
+++ helpers/api-util.js
@@ -0,0 +1,17 @@
+export async function getAllEvents() {
+  const response = fetch(
+    "https://nextjs-course-70e3e-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
